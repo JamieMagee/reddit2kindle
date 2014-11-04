fc993a21bd6251f68dbc25002ca4fb4fea715620
Jamie Magee <jamie.magee@gmail.com>
2014-11-04 22:15:01 +0100

    Initial commit

---
+++ reddit2kindle.py
@@ -0,0 +1,58 @@
+from flask import Flask, request, redirect, url_for, flash
+from flask.templating import render_template
+import util
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/thread', methods=['POST'])
+def thread():
+    if util.validate_request(request.form) is not None:
+        flash(util.validate_request(request.form), 'danger')
+        return redirect(url_for('index'))
+
+    try:
+        submission = util.r.get_submission(url=request.form['submission'])
+    except:
+        flash('That wasn\'t a reddit link, was it?', 'danger')
+        return redirect(url_for('index'))
+    submission.replace_more_comments(limit=0)
+
+    comments = util.get_comments(submission)
+    body = util.markdown(submission.selftext, output_format='html5')
+    title = submission.title
+    author = submission.author.name
+    address = request.form['email']
+
+    attachment = render_template('comments.html', title=title, body=body, author=author, comments=comments)
+
+    status, msg = util.send_email(address, attachment, title)
+
+    if status == 200:
+        flash('Success!', 'success')
+        return redirect(url_for('index'))
+    else:
+        flash('Uh oh! Something went wrong on our end', 'warning')
+        return redirect(url_for('index'))
+
+
+@app.route('/top', methods=['POST'])
+def convert():
+    subreddit = util.r.get_subreddit(request.form['subreddit'])
+    time = request.form['time']
+    limit = int(request.form['limit'])
+
+    subreddit_data = util.get_posts(subreddit, time, limit)
+    results = util.parse_subreddit_data(subreddit_data)
+
+    return render_template('results.html', results=results, subreddit=subreddit, time=time, limit=limit)
+
+if __name__ == '__main__':
+    app.secret_key = os.urandom(24)
+    app.run(debug=True)
---
+++ util.py
@@ -0,0 +1,70 @@
+import praw
+from markdown import markdown
+import sendgrid
+import os
+from configparser import ConfigParser
+
+
+def to_html(comment):
+    result = markdown(comment.body) + '<footer>' + comment.author.name + '</footer>'
+    children = ['<blockquote>' + to_html(reply) + '</blockquote>' for reply in comment.replies if
+                reply.author is not None]
+    if children:
+        result += ''.join(children)
+    return result
+
+
+def get_comments(submission):
+    out = ''
+    for comment in submission.comments:
+        if comment.author is not None:
+            out += '<blockquote>' + to_html(comment) + '</blockquote>'
+    return out
+
+
+def get_auth():
+    if os.path.isfile(os.path.join(os.path.dirname(__file__), 'settings.cfg')):
+        config = ConfigParser()
+        config.read(os.path.join(os.path.dirname(__file__), 'settings.cfg'))
+        username = config.get('auth', 'username')
+        password = config.get('auth', 'password')
+    else:
+        username = os.environ['SENDGRID_USERNAME']
+        password = os.environ['SENDGRID_PASSWORD']
+    return username, password
+
+
+def send_email(to, attachment, title):
+    message = sendgrid.Mail()
+    message.add_to(to)
+    message.set_subject(title)
+    message.set_text(' ')
+    message.set_from('reddit2kindle')
+    message.add_attachment_stream('{}.html'.format(title), attachment)
+    return sg.send(message)
+
+
+def validate_request(values):
+    if values['submission'] is '':
+        return 'You need to put a URL in!'
+    if values['email'] is '':
+        return 'How am I supposed to send it to you without an email address?'
+    return None
+
+r = praw.Reddit(user_agent='reddit2kindle')
+sg = sendgrid.SendGridClient(get_auth()[0], get_auth()[1])
+
+
+def get_posts(subreddit, time, limit):
+    if time == 'hour':
+        return subreddit.get_top_from_hour(limit=limit)
+    elif time == 'day':
+        return subreddit.get_top_from_day(limit=limit)
+    elif time == 'week':
+        return subreddit.get_top_from_week(limit=limit)
+    elif time == 'month':
+        return subreddit.get_top_from_month(limit=limit)
+    elif time == 'year':
+        return subreddit.get_top_from_year(limit=limit)
+    elif time == 'all':
+        return subreddit.get_top_from_all(limit=limit)
