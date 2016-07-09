aa6e5e93406cc614d1935f0ee61f28dbc955c2c0
Jamie Magee <jamie.magee@gmail.com>
2016-07-09 21:13:27 +0200

    Make numbers the default comments style

--- forms.py
+++ forms.py
@@ -8,7 +8,7 @@ csrf = CsrfProtect()
 class Submission(Form):
     submission = StringField('Submission URL')
     comments = BooleanField('Include comments')
-    comments_style = SelectField('Comments style', choices=[('quotes', 'quotes'), ('numbers', 'numbers')])
+    comments_style = SelectField('Comments style', choices=[('numbers', 'numbers'), ('quotes', 'quotes')])
     email = StringField('Kindle email address')
     kindle_address = SelectField('Kindle address', choices=[('normal', '@kindle.com'), ('free', '@free.kindle.com')])
 
@@ -16,7 +16,7 @@ class Submission(Form):
 class Subreddit(Form):
     subreddit = StringField('Subreddit')
     comments = BooleanField('Include comments')
-    comments_style = SelectField('Comments style', choices=[('quotes', 'quotes'), ('numbers', 'numbers')])
+    comments_style = SelectField('Comments style', choices=[('numbers', 'numbers'), ('quotes', 'quotes')])
     time = SelectField('Time period',
                        choices=[('all', 'all'), ('year', 'year'), ('month', 'month'), ('week', 'week'), ('day', 'day'),
                                 ('hour', 'hour')])
